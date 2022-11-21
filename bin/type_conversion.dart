79ff012b2a69c2d98e34ffd5772abd7404f995f8
Kelvin Anderson <kelvinbusiness10@gmail.com>
2022-11-21 17:16:33 +0700

    [ADD] type conversion and error checking

---
+++ bin/type_conversion.dart
@@ -0,0 +1,16 @@
+void main() {
+  var inputString = '55555';
+  String formatedString = formatStringToInt(inputString);
+  print(formatedString);
+}
+
+String formatStringToInt(String inputString) {
+  try {
+    var inputInt = int.parse(inputString);
+    return "convertion success";
+  } on FormatException {
+    return 'invalid string, conversion failed';
+  } catch (e) {
+    return 'convertion failed';
+  }
+}
